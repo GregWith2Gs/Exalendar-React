1db6d7ffbf58ba83f689f4610c8a1c798d0ff2a3
aaselvam <selvaa@rpi.edu>
2023-02-24 17:25:07 -0500

    create event form (doesn't work)

--- exalendar/src/components/App.js
+++ exalendar/src/components/App.js
@@ -1,11 +1,10 @@
 import Home from './Home.js'
-import ReactBigCalendar from './ReactBigCalendar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ReactBigCalendar />
+        <Home/>
       </header>
     </div>
   );
--- exalendar/src/components/Home.js
+++ exalendar/src/components/Home.js
@@ -1,6 +1,61 @@
+import ReactBigCalendar from './ReactBigCalendar';
+import React, { useState } from "react";
+
 function Home() {
+    const [eventName, setEventName] = useState('');
+    const [eventType, setEventType] = useState('');
+    const [eventDesc, setEventDesc] = useState('');
+    const [eventStart, setEventStart] = useState('');
+    const [eventEnd, setEventEnd] = useState('');
+
+    function submitEvent() {
+        console.log(eventName);
+        fetch('http://localhost:4000/', {
+            method: 'POST',
+            body: JSON.stringify({
+                'name': eventName,
+                'type': eventType,
+                'event_description': eventDesc,
+                'event_date_start': eventStart,
+                'event_date_end': eventEnd
+            })
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            // Handle data
+        })
+    }
+
     return (
-        <h1>Home Page</h1>
+        <div>
+            <h1>Home Page</h1>
+            <ReactBigCalendar />
+            <br></br>
+            <form>
+                <label>
+                    Event name:
+                    <input type="text" value={eventName} onChange={(e)=>setEventName(e.target.value)}/>
+                </label>
+                <label>
+                    Event type:
+                    <input type="text" value={eventType} onChange={(e)=>setEventType(e.target.value)}/>
+                </label>
+                <label>
+                    Event description:
+                    <input type="text" value={eventDesc} onChange={(e)=>setEventDesc(e.target.value)}/>
+                </label>
+                <label>
+                    Event start time:
+                    <input type="text" value={eventStart} onChange={(e)=>setEventStart(e.target.value)}/>
+                </label>
+                <label>
+                    Event end time:
+                    <input type="text" value={eventEnd} onChange={(e)=>setEventEnd(e.target.value)}/>
+                </label>
+                <input type="submit" onSubmit={submitEvent}/>
+            </form>
+        </div>
     );
 }
 

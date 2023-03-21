0394d347538118023744da7d38a871aec0a6b768
aaselvam <selvaa@rpi.edu>
2023-03-21 16:59:47 -0400

    logo, login, exalnder sql

--- exalendar/src/components/App.js
+++ exalendar/src/components/App.js
@@ -1,9 +1,11 @@
 import Home from './Home.js'
+import Login from './Login.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/* <Login/> */}
         <Home/>
       </header>
     </div>

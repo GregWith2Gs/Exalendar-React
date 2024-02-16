7d0def35429abafe9d51c38ae4b1bab8824aa78c
Greg Roper <roperg@rpi.edu>
2024-02-16 17:37:59 -0500

    oh okay that was easy
    this works! Backend connected!

--- src/server.js
+++ src/server.js
@@ -45,8 +45,8 @@ app.post("/", (req, res) => {
     res.send('Data Received: ' + JSON.stringify(data));
     const sql_code = `INSERT INTO events (class_name, event_title, event_type, event_description, event_location, event_start, event_end, event_freq, event_end_date, event_interval, event_byday) VALUES (?,?,?,?,?,?,?,?,?,?,?)`;
 
-      connection.query(sql_code, [data.className, data.eName, data.eType, data.eDescription, data.eLocation, data.eStart, data.eEnd, 
-        data.eFreq, data.eEndDate, data.eInterval, data.eByDay], function (err, results, fields) {
+      connection.query(sql_code, [data.className, data.name, data.type, data.description, data.location, data.start, data.end, 
+        data.freq, data.end_date, data.interval, data.byday], function (err, results, fields) {
         if (err) throw err;
         console.log(results);
     });

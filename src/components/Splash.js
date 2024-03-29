2400d49457b7ced225b0af148b464018a71a7ba8
markbot2000 <Markbot2000@gmail.com>
2024-03-29 17:18:15 -0400

    new css file

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import React from 'react';
 import {Navigate} from 'react-router-dom';
+import '../css/Splash.css';
 
 
 function Splash(){
@@ -9,35 +10,14 @@ function Splash(){
     
     
     
-        <div className="App text-center container-fluid">
+        <div className="Splash text-center container-fluid">
           {!localStorage.loggedIn ? (
-            <div 
-            style={{
-            margin: '300px auto',
-            'max-width': '400px',
-            display: 'flex',
-            'flex-direction': 'column',
-            'align-items': 'center',
-            'font-family': 'sans-serif',
-            }}
-        >
+            <div>
     
           <h3>Welcome to Discord OAuth NodeJS App</h3>
           <p>Click on the below button to get started!</p>
-          <a 
+          <a classname="Login-box"
               href="https://discord.com/oauth2/authorize?client_id=1217203680294469695&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fdiscord&scope=email+identify"
-              style={{
-                outline: 'none',
-                padding: '10px',
-                border: 'none',
-                'font-size': '20px',
-                'margin-top': '20px',
-                'border-radius': '8px',
-                background: '#6D81CD',
-                cursor:'pointer',
-                'text-decoration': 'none',
-                color: 'white',
-              }}
           >
           Login with Discord</a>
       </div>

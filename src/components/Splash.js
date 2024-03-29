b9d44eb7b62cffc16971930739f0330fd2940a5d
markbot2000 <Markbot2000@gmail.com>
2024-03-29 17:25:22 -0400

    fixed typo and css issues

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -16,13 +16,13 @@ function Splash(){
     
           <h3>Welcome to Discord OAuth NodeJS App</h3>
           <p>Click on the below button to get started!</p>
-          <a classname="Login-box"
+          <a className="Login-box"
               href="https://discord.com/oauth2/authorize?client_id=1217203680294469695&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fdiscord&scope=email+identify"
           >
           Login with Discord</a>
       </div>
           ) : (
-            <Navigate to='/ReactBigCalendar' />
+            <Navigate to='/Calendar' />
           )}
         </div>
       );
